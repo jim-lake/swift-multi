4f2f68a0e8f0195f72dba6368c2f4428a1c071f1
jlake <jim@blueskylabs.com>
2021-01-15 08:29:34 -0800

    cleanup yargs and npm

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 'use strict';
 
-const { dirname } = require('path');
-const { argv } = require('yargs');
+const yargs = require('yargs');
 const SwiftMulti = require('./index');
 const { env } = process;
 
 const SEND_PER_IP_DEFAULT = 3;
 
-const command_path = process.argv[1];
-
-function usage() {
-  const name = command_path.slice(dirname(command_path).length + 1);
-  console.error(
-    `Usage: ${name} <upload|download> <container> <source_path>
+const USAGE = `Usage: $0 <upload|download> <container> <source_path>
   [--object_path object_path]
   [--os_auth_url url]
   [--os_password password]
@@ -22,10 +16,9 @@ function usage() {
   [--send_per_ip count]
   [--ip_list list]
   [--delete_at unixtime]
-  [--delete_after seconds]
-`
-  );
-}
+  [--delete_after seconds]`;
+
+const argv = yargs.usage(USAGE).argv;
 
 const method = argv._[0];
 const container = argv._[1];
@@ -45,23 +38,27 @@ if (!delete_at && delete_after) {
 }
 
 if (argv.help) {
-  usage();
+  yargs.showHelp("log");
   process.exit(-1);
 }
 if (!os_auth_url || !os_password || !os_tenant_name || !os_username) {
   console.error('No auth found.');
   process.exit(-254);
 }
+if (!method) {
+  yargs.showHelp("log");
+  process.exit(-253);
+}
 if (method !== 'upload' && method !== 'download') {
   console.error('Invalid method, provide upload or download.');
   process.exit(-253);
 }
 if (!source_path) {
-  usage();
+  yargs.showHelp("log");
   process.exit(-1);
 }
 if (!container) {
-  usage();
+  yargs.showHelp("log");
   process.exit(-1);
 }
 
@@ -69,10 +66,10 @@ if (ip_list.length === 1 && !ip_list[0].length) {
   ip_list.splice(0, 1);
 }
 if (ip_list && ip_list.length > 1) {
-  _consoleLog('Source IPs:', ip_list);
+  console.log('Source IPs:', ip_list);
 }
 if (delete_at) {
-  _consoleLog('Delete at:', delete_at);
+  console.log('Delete at:', delete_at);
 }
 
 if (method === 'upload') {
